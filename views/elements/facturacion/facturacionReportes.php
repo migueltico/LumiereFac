480a195a82802abdd6189965b08225dd66057ac4
DESKTOP-KBA4FG6\migue <mesquivel@bminc.eu>
2021-02-12 10:00:03 -0600

    AJUSTE

--- views/elements/facturacion/facturacionReportes.php
+++ views/elements/facturacion/facturacionReportes.php
@@ -19,7 +19,33 @@
                 </div>
             </div>
         </div>
-        <?php include(self::block('reportes/ReporteFacturasPorDia')) ?>
+        <div class="row optionselectReports" id="rxfacDia" style="display: none;">
+            <div class="row col">
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-1">
+                    <div class="input-group flex-nowrap">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="addon-wrapping">Fecha Inicio</span>
+                        </div>
+                        <input id="dateInit" type="date" class="form-control" value="<?= date('Y-m-d'); ?>">
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-1">
+                    <div class="input-group flex-nowrap">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="addon-wrapping">Fecha Final</span>
+                        </div>
+                        <input id="dateEnd" type="date" class="form-control" value="<?= date('Y-m-d'); ?>">
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-1">
+                    <button data-type="html" class="btn btn-primary generarReportesFac">Generar</button>
+                    <button data-type="pdf" class="btn btn-info generarReportesFac">PDF</button>
+                </div>
+            </div>
+
+        </div>
+        <?php //include(self::block('reportes/ReporteFacturasPorDia')) 
+        ?>
     </div>
     <div class="card-body loadTable" id="loadTable">
     </div>
